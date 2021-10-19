cfa6449eea08481d115af0eacc13c1a77c483446
김민지 <90051822+mjikim@users.noreply.github.com>
2021-10-20 02:41:44 +0900

    add

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'x-eg)=t0xum#bpg5dqtn4addsj_-fw4xp(z=#h%fuk@l26wfuf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
